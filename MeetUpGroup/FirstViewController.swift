ddcaccf1561120d3765d6a9ea70277f0b6f481bf
ChongSun <Az@192-168-1-2.tpgi.com.au>
2016-03-29 20:22:21 +1100

    Initial Commit

---
+++ MeetUpGroup/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MeetUpGroup
+//
+//  Created by ChongSun on 29/03/2016.
+//  Copyright © 2016 RMIT. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
